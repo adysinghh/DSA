8b40fff775fe02be6365b74d86be95ddbc1b32af
Aditya Singh <aditya12345rajput56@gmail.com>
2025-06-22 11:48:58 +0530

    .

--- leetcode/lc485.java
+++ leetcode/lc485.java
@@ -1,3 +1,18 @@
-public class lc485 {
-    
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int lcount = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 1) {
+                lcount++;
+                if(lcount > count){
+                    count = lcount;
+                }
+            } else {
+                lcount = 0;
+            }
+        }
+        return count;
+    }
 }
