7bdf1748898da9c82e0d15e0d0a5af78fae82709
Aditya Singh <aditya12345rajput56@gmail.com>
2025-06-28 16:28:56 +0530

    Rotate Image

---
+++ leetcode/lc48.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        for(int i = 0; i<matrix.length-1; i++){
+            for(int j = i+1; j<matrix.length; j++) {
+                // swap(matrix[i][j], matrix[j][i]);
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            reverseRow(matrix[i]);
+        }
+    }
+
+    private void reverseRow(int[] row) {
+        int left = 0, right = row.length - 1;
+        while (left < right) {
+            int temp = row[left];
+            row[left] = row[right];
+            row[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
