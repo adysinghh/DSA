5f96b8d45bee4ddbd5d465d6894886818f4e076d
Aditya Singh <aditya12345rajput56@gmail.com>
2025-08-28 17:26:59 +0530

    Largest Odd Number in String

---
+++ leetcode/lc1903.java
@@ -0,0 +1,18 @@
+// 1903. Largest Odd Number in String
+
+class Solution {
+    public String largestOddNumber(String num) {
+    
+        int n = num.length();
+
+        for(int i = n-1; i>=0; i--) {
+            char c = num.charAt(i);
+            int digit = c - '0';
+
+            if(digit%2 == 1) {
+                return num.substring(0, i+1);
+            }
+        }
+        return "";
+    }
+}
