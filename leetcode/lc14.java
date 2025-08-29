2d7bdfcb8f314024788e1acc09d387cd31ee8c26
Aditya Singh <aditya12345rajput56@gmail.com>
2025-08-29 17:43:03 +0530

    updated Longest Common Prefix

--- leetcode/lc14.java
+++ leetcode/lc14.java
@@ -3,12 +3,14 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         StringBuilder result = new StringBuilder();
-        int n = strs.length;
+        
 
         Arrays.sort(strs);
 
         char[] firstElem = strs[0].toCharArray();
-        char[] lastElem  = strs[n-1].toCharArray();
+        char[] lastElem  = strs[strs.length-1].toCharArray();
+
+        int n = firstElem.length;
 
         for (int i = 0; i < n; i++) {
             if (firstElem[i] != lastElem[i]) break;
