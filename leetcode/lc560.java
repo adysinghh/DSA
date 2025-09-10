5af2ce0bd3dbe34cd755190f4b26b67e6fbe4878
Aditya Singh <aditya12345rajput56@gmail.com>
2025-09-10 13:25:09 +0530

    Subarray Sum Equals K

---
+++ leetcode/lc560.java
@@ -0,0 +1,21 @@
+// 560. Subarray Sum Equals K
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int sum = 0;
+
+        for(int i = 0; i<nums.length; i++) {
+            for(int j = i; j<nums.length; j++) {
+                // if (nums[j] == k) count++;
+                sum += nums[j];
+                if (sum == k) {
+                    count++;
+                }
+            }
+            sum = 0;
+        }
+
+        return count;
+    }
+}
