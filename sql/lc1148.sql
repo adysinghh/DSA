e2810cf665f8d330eb05b9bb6dcaab62b24bcf59
Aditya Singh <aditya12345rajput56@gmail.com>
2025-09-18 13:59:11 +0530

    added context

--- sql/lc1148.sql
+++ sql/lc1148.sql
@@ -1,7 +1,7 @@
 -- 1148. Article Views I
 
 # Write your MySQL query statement below
-SELECT DISTINCT author_id AS id -- since we need new table with column name id
+SELECT DISTINCT author_id AS id -- since we need new table with column name id, [AS] - to rename
 FROM Views
 WHERE author_id = viewer_id -- to check if author viewed his own article
 ORDER BY author_id ASC; -- to sort in ascending order
